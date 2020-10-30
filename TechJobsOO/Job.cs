8ff8d1ce4defc5e6c1c7fdc72dd92d417a24f0f8
Hanna Sumpter <sumpterhanna@gmail.com>
2020-10-29 21:24:12 -0500

    Completed Job Class

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+
 namespace TechJobsOO
 {
     public class Job
@@ -14,6 +16,29 @@ namespace TechJobsOO
 
         // TODO: Add the two necessary constructors.
 
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
+        public Job(string name, string employerName, string employerLocation, string jobType, string jobCoreCompetency) : this()
+        {
+            Name = name;
+            EmployerName = new Employer(employerName.ToString());
+            EmployerLocation = new Location(employerLocation.ToString());
+            JobType = new PositionType(jobType.ToString());
+            JobCoreCompetency = new CoreCompetency(jobCoreCompetency.ToString());
+        }
         // TODO: Generate Equals() and GetHashCode() methods.
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
     }
 }
